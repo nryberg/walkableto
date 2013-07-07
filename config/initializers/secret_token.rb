d29a69332a6e01e2726328d3c26a2ac3e3c291a6
Nick Ryberg <nryberg@gmail.com>
2013-07-06 21:47:22 -0500

    First commit for walkeable to

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Walkableto::Application.config.secret_token = '897ef802d8f0bf8a235f4928ac3304dcf6ed448e29d6a9d71ae0dd8fb979a0f209485e0fccdd4b607ae55e4d670d0ac2872559f3c64699aa015bb8f5945c2553'
